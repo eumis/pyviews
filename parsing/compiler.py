d1f30a6df5f41bf0a7aa82ffebcef30ca02cd33d
Eugen Misievich <misievich@gmail.com>
2017-05-24 13:14:02 +0300

    refactoring

--- parsing/compiler.py
+++ parsing/compiler.py
@@ -4,18 +4,17 @@ def compile_view(node):
     return compile_node(node)
 
 def compile_page(node, parent):
-    for child in list(node):
-        compile_widget(child, parent)
+    compile_children(node, parent)
 
 def compile_widget(node, parent):
     widget = compile_node(node, parent)
+    apply_attributes(node, widget)
+    apply_text(node, widget)
+    compile_children(node, widget)
+
+def apply_attributes(node, widget):
     for attr in node.items():
         apply_attr(widget, attr)
-    text = node.text.strip()
-    if text:
-        widget.configure(text=text)
-    for child in list(node):
-        compile_widget(child, widget)
 
 def apply_attr(widget, attr):
     if not hasattr(widget, attr[0]):
@@ -31,6 +30,15 @@ def apply_method(widget, attr):
 
 def apply_property(widget, attr):
     widget.__dict__[attr[0]] = attr[1]
+
+def apply_text(node, widget):
+    text = node.text.strip()
+    if text:
+        widget.configure(text=text)
+
+def compile_children(node, widget):
+    for child in list(node):
+        compile_widget(child, widget)
 
 def compile_node(node, *args):
     # tag = "{namespace}name"
