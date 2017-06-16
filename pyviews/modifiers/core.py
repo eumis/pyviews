a45ca10072bf94d68a6be009888c134b078a6613
Eugen Misievich <misievich@gmail.com>
2017-06-16 18:28:54 +0300

    hohoho;

--- pyviews/modifiers/core.py
+++ pyviews/modifiers/core.py
@@ -3,19 +3,19 @@ from pyviews.binding.expressions import split_by_last_dot
 
 def import_global(node, attr):
     (name, path) = attr
-    imported = import_path(path)
+    imported = _import_path(path)
     if imported:
         node.context.globals[name] = imported
 
-def import_path(path):
+def _import_path(path):
     if not path:
         return None
     try:
         return import_module(path)
-    except ImportError:
-        pass
+    except ImportError as exception:
+        print(str(exception))
     (path, name) = split_by_last_dot(path)
-    module = import_path(path)
+    module = _import_path(path)
     return module.__dict__[name] if module else None
 
 def bind(node, attr):
