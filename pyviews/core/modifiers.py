181b545be9d44091813dcaf66a074534634af521
Eugen Misievich <misievich@gmail.com>
2017-09-08 15:51:04 +0300

    refactoring

--- pyviews/core/modifiers.py
+++ pyviews/core/modifiers.py
@@ -1,19 +1,7 @@
-from importlib import import_module
-from pyviews.common.expressions import split_by_last_dot
+from pyviews.core.reflection import import_path
 
 def import_(node, attr):
     (name, path) = attr
-    imported = _import_path(path)
+    imported = import_path(path)
     if imported is not None:
         node.context.globals[name] = imported
-
-def _import_path(path):
-    if not path:
-        return None
-    try:
-        return import_module(path)
-    except ImportError as exception:
-        print(str(exception))
-    (path, name) = split_by_last_dot(path)
-    module = _import_path(path)
-    return module.__dict__[name] if module else None
