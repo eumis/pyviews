ebdd75252a9b5cd75f99980ac38ef12ec20ccd64
Eugen Misievich <misievich@gmail.com>
2017-09-21 16:12:41 +0300

    added binding tests

--- pyviews/core/binding.py
+++ pyviews/core/binding.py
@@ -4,14 +4,14 @@ from pyviews.core.viewmodel import ViewModel
 class PropertyGetRecorder:
     def __init__(self, view_model: ViewModel):
         self._view_model = view_model
-        self._used_props = []
+        self._used_props = set()
         self._recorders = {}
 
     def __getattribute__(self, prop):
         if prop in ['_view_model', '_used_props', '_recorders', 'get_used_properties']:
             return object.__getattribute__(self, prop)
         if self._view_model is not None:
-            self._used_props.append(prop)
+            self._used_props.add(prop)
             value = getattr(self._view_model, prop)
             if isinstance(value, ViewModel):
                 if value not in self._recorders:
@@ -21,9 +21,14 @@ class PropertyGetRecorder:
 
     def get_used_properties(self):
         used_props = {self._view_model: self._used_props}
-        for recorder in self._recorders.values():
-            used_props.update(recorder.get_used_properties())
-        return used_props
+        return _merge_used_properties(used_props, self._recorders.values())
+
+def _merge_used_properties(used_props, recorders):
+    for recorder in recorders:
+        for vml, props in recorder.get_used_properties().items():
+            used_props[vml] = used_props[vml].union(props) if vml in used_props \
+                                 else props
+    return {vml: props for vml, props in used_props.items() if props}
 
 class Binding:
     def __init__(self, instance, prop, modifier, expression: Expression):
@@ -32,9 +37,10 @@ class Binding:
         self.property = prop
         self.modifier = modifier
         self.dependencies = []
+        self._init()
 
     def _init(self):
-        parameters = self._get_record_parameters()()
+        parameters = self._get_record_parameters()
         value = self.get_value(parameters)
         used_properties = self._get_used_properties(parameters)
         self._create_dependencies(used_properties)
@@ -50,23 +56,18 @@ class Binding:
     def _get_used_properties(self, parameters):
         recorders = [value for value in parameters.values() \
                      if isinstance(value, PropertyGetRecorder)]
-        used_properties = {}
-        for recorder in recorders:
-            for view_model, props in recorder.items():
-                if view_model not in used_properties:
-                    used_properties[view_model] = set()
-                used_properties[view_model].update(props)
+        return _merge_used_properties({}, recorders)
 
     def _create_dependencies(self, used_properties):
         for view_model, props in used_properties.items():
             for prop in props:
-                view_model.observe(prop, self.update_callback)
+                view_model.observe(prop, self._update_callback)
                 self.dependencies.append((view_model, prop))
 
     def get_value(self, parameters=None):
         return self._expression.compile(parameters)
 
-    def update_callback(self, new_val, old_val):
+    def _update_callback(self, new_val, old_val):
         self.update_prop()
 
     def update_prop(self):
@@ -74,4 +75,4 @@ class Binding:
 
     def destroy(self):
         for view_model, prop in self.dependencies:
-            view_model.release_callback(prop, self.update_callback)
+            view_model.release_callback(prop, self._update_callback)
--- tests/core/binding_test.py
+++ tests/core/binding_test.py
@@ -1,5 +1,7 @@
 from unittest import TestCase, main
-from pyviews.core.binding import PropertyGetRecorder
+from tests.utility import case
+from pyviews.core.binding import PropertyGetRecorder, Binding
+from pyviews.core.compilation import Expression
 from pyviews.core.viewmodel import ViewModel
 
 class InnerViewModel(ViewModel):
@@ -8,26 +10,28 @@ class InnerViewModel(ViewModel):
         self.int_value = int_value
         self.str_value = str_value
 
-class TestViewModel(ViewModel):
+class ParentViewModel(ViewModel):
     def __init__(self, int_value, inner_vm):
         super().__init__()
         self.int_value = int_value
         self.inner_vm = inner_vm
+        self.another_vm = inner_vm
 
 class TestPropertyGetRecorder(TestCase):
     def setUp(self):
         self.inner_vm = InnerViewModel(1, 'str_value')
-        self.vm = TestViewModel(1, self.inner_vm)
-        self.recorder = PropertyGetRecorder(self.vm)
+        self.view_model = ParentViewModel(1, self.inner_vm)
+        self.recorder = PropertyGetRecorder(self.view_model)
 
-    def test_properties(self):        
+    def test_properties(self):
         msg = 'recorder should return values from vm'
-        self.assertEqual(self.recorder.int_value, self.vm.int_value, msg)
-        self.assertEqual(self.recorder.inner_vm.int_value, self.vm.inner_vm.int_value, msg)
-        self.assertEqual(self.recorder.inner_vm.str_value, self.vm.inner_vm.str_value, msg)
+        self.assertEqual(self.recorder.int_value, self.view_model.int_value, msg)
+        self.assertEqual(self.recorder.inner_vm.int_value, self.view_model.inner_vm.int_value, msg)
+        self.assertEqual(self.recorder.inner_vm.str_value, self.view_model.inner_vm.str_value, msg)
 
     def test_inner_vm(self):
-        self.assertIsInstance(self.recorder.inner_vm, PropertyGetRecorder, 'recorder should replace inner ViewModel by recorder')
+        msg = 'recorder should replace inner ViewModel by recorder'
+        self.assertIsInstance(self.recorder.inner_vm, PropertyGetRecorder, msg)
 
     def test_get_used_properties(self):
         self.recorder.int_value
@@ -35,16 +39,110 @@ class TestPropertyGetRecorder(TestCase):
 
         used_properties = self.recorder.get_used_properties()
 
-        msg = 'only view models with called properties should be returned'
-        self.assertEqual(len(used_properties), 2, msg)
-        self.assertTrue(self.vm in used_properties, msg)
+        msg = 'view models with called properties should be returned'
+        self.assertTrue(self.view_model in used_properties, msg)
         self.assertTrue(self.inner_vm in used_properties, msg)
 
-        msg = 'only called properties should be returned'
-        self.assertTrue('int_value' in used_properties[self.vm], msg)
-        self.assertTrue('inner_vm' in used_properties[self.vm], msg)
-
+        msg = 'called properties should be returned'
+        self.assertTrue('int_value' in used_properties[self.view_model], msg)
+        self.assertTrue('inner_vm' in used_properties[self.view_model], msg)
         self.assertTrue('str_value' in used_properties[self.inner_vm], msg)
+
+    def test_properties_duplicates(self):
+        self.recorder.int_value
+        self.recorder.int_value
+        self.recorder.inner_vm
+        self.recorder.inner_vm
+
+        used_properties = self.recorder.get_used_properties()
+
+        msg = 'only view models with called properties should be returned'
+        self.assertEqual(len(used_properties), 1, msg)
+        self.assertTrue('inner_vm' in used_properties[self.view_model], msg)
+
+        msg = 'only called properties should be returned'
+        self.assertEqual(len(used_properties[self.view_model]), 2, msg)
+        self.assertTrue('int_value' in used_properties[self.view_model], msg)
+        self.assertTrue('inner_vm' in used_properties[self.view_model], msg)
+
+class TestBindable:
+    def __init__(self):
+        self.int_value = 1
+        self.str_value = 'str'
+
+class TestBinding(TestCase):
+    @case('2 + 2', None, 4)
+    @case('some_key', {'some_key': 1}, 1)
+    @case('some_key - 1', {'some_key': 1}, 0)
+    @case('str(some_key)', {'some_key': 1}, '1')
+    @case('some_key', {'some_key': 'asdf'}, 'asdf')
+    @case('some_key(some_value)', {'some_key': lambda val: val, 'some_value': 'value'}, 'value')
+    def test_get_value(self, code, params, expected):
+        expression = Expression(code, params)
+        inst = TestBindable()
+        binding = Binding(inst, 'int_value', setattr, expression)
+
+        msg = 'get_value should return expression result value'
+        self.assertEqual(binding.get_value(), expected, msg)
+
+    def test_get_value_parameters(self):
+        expression = Expression('(one, two)', {'one': 1, 'two': 2})
+        inst = TestBindable()
+        binding = Binding(inst, 'int_value', setattr, expression)
+
+        msg = 'get_value should return expression result value'
+        expected = ('one value', 'two value')
+        self.assertEqual(binding.get_value({'one': 'one value', 'two': 'two value'}), expected, msg)
+
+    def test_update_prop(self):
+        view_model = InnerViewModel(2, 'inner str')
+        expression = Expression('str(vm.int_value) + vm.str_value', {'vm': view_model})
+        inst = TestBindable()
+        binding = Binding(inst, 'str_value', setattr, expression)
+
+        binding.update_prop()
+
+        msg = 'update_prop should compile expression and set result to binded property'
+        self.assertEqual(inst.str_value, str(view_model.int_value) + view_model.str_value, msg)
+
+    def test_binding_creation(self):
+        view_model = InnerViewModel(2, 'inner str')
+        expression = Expression('str(vm.int_value) + vm.str_value', {'vm': view_model})
+        inst = TestBindable()
+        Binding(inst, 'str_value', setattr, expression)
+
+        msg = 'property should be updated on binding creation'
+        self.assertEqual(inst.str_value, str(view_model.int_value) + view_model.str_value, msg)
+
+    def test_binding(self):
+        view_model = InnerViewModel(2, 'inner str')
+        expression = Expression('str(vm.int_value) + vm.str_value', {'vm': view_model})
+        inst = TestBindable()
+        Binding(inst, 'str_value', setattr, expression)
+
+        msg = 'property should be updated on vm change'
+
+        view_model.int_value = 3
+        self.assertEqual(inst.str_value, str(view_model.int_value) + view_model.str_value, msg)
+
+        view_model.str_value = 'new str value'
+        self.assertEqual(inst.str_value, str(view_model.int_value) + view_model.str_value, msg)
+
+    def test_destroy(self):
+        view_model = InnerViewModel(2, 'inner str')
+        expression = Expression('str(vm.int_value) + vm.str_value', {'vm': view_model})
+        inst = TestBindable()
+        binding = Binding(inst, 'str_value', setattr, expression)
+        expected = inst.str_value
+        binding.destroy()
+
+        msg = 'destroy should remove property change on expression change'
+
+        view_model.int_value = 3
+        self.assertEqual(inst.str_value, expected, msg)
+
+        view_model.str_value = 'new str value'
+        self.assertEqual(inst.str_value, expected, msg)
 
 if __name__ == '__main__':
     main()
