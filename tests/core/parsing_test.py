7ac3c0fc5707a20da701d79d42942db8927be485
Eugen Misievich <misievich@gmail.com>
2017-09-26 13:59:45 +0300

    added globals tests

--- tests/core/parsing_test.py
+++ tests/core/parsing_test.py
@@ -1,7 +1,52 @@
 from unittest import TestCase, main
 from xml.etree import ElementTree as ET
 from pyviews.core.xml import XmlNode
-from pyviews.core.parsing import Node
+from pyviews.core.parsing import Globals, Node
+
+class TestGlobals(TestCase):
+    def test_globals_keys(self):
+        parent = Globals()
+        parent['one'] = 1
+        parent['two'] = 2
+
+        globs = Globals(parent)
+        globs['two'] = 'two'
+        globs['three'] = 'three'
+
+        self.assertEqual(globs['one'], 1, 'Globals should get values from parent')
+        msg = 'Globals should get own value if key exists'
+        self.assertEqual(globs['two'], 'two', msg)
+        self.assertEqual(globs['three'], 'three', msg)
+
+    def test_parent_keys(self):
+        parent = Globals()
+        parent['one'] = 1
+        parent['two'] = 2
+
+        globs = Globals(parent)
+        globs['two'] = 'two'
+        globs['three'] = 'three'
+
+        msg = 'own_keys should return only own keys'
+        self.assertEqual(sorted(globs.own_keys()), sorted(['two', 'three']), msg)
+
+        msg = 'all_keys should return own keys plus parent keys'
+        self.assertEqual(sorted(globs.all_keys()), sorted(['one', 'two', 'three']), msg)
+
+    def test_dictionary(self):
+        parent = Globals()
+        parent['one'] = 1
+        parent['two'] = 2
+
+        globs = Globals(parent)
+        globs['two'] = 'two'
+        globs['three'] = 'three'
+
+        msg = 'to_dictionary should return dictionary with own keys'
+        self.assertEqual(globs.to_dictionary(), {'two': 'two', 'three': 'three'}, msg)
+
+        msg = 'to_all_dictionary should return dictionary with all keys'
+        self.assertEqual(globs.to_all_dictionary(), {'one': 1, 'two': 'two', 'three': 'three'}, msg)
 
 class TestNode(TestCase):
     def test_init(self):
@@ -11,66 +56,6 @@ class TestNode(TestCase):
 
         msg = 'Node should inititalise properties from init parameters'
         self.assertEqual(node.xml_node, xml_node, msg)
-
-# class TestDictionary_(TestCase):
-#     def test_dictionary_init(self):
-#         dictionary = HierarchyDict()
-
-#         self.assertEqual(len(dictionary), 0, 'dictionary length by default should be 0')
-
-#     def test_dictionary_keys(self):
-#         dictionary = HierarchyDict()
-
-#         dictionary['one'] = 'one'
-#         dictionary['two'] = 2
-
-#         msg = 'Dictionary should get and set values by key'
-#         self.assertEqual(dictionary['one'], 'one', msg)
-#         self.assertEqual(dictionary['two'], 2, msg)
-
-#         self.assertEqual(len(dictionary), 2, 'dictionary length should be equal to keys count')
-
-#     def test_dictionary_parent(self):
-#         parent = HierarchyDict()
-#         parent['one'] = 1
-#         parent['two'] = 2
-
-#         dictionary = HierarchyDict(parent)
-#         dictionary['two'] = 'two'
-#         dictionary['three'] = 'three'
-
-#         self.assertEqual(dictionary['one'], 1, 'Dictionary should get values from parent')
-#         msg = 'Dictionary should get own value if key exists'
-#         self.assertEqual(dictionary['two'], 'two', msg)
-#         self.assertEqual(dictionary['three'], 'three', msg)
-
-#         msg = 'dictionary length should be equal to keys count including parent'
-#         self.assertEqual(len(dictionary), 3, msg)
-
-#     def test_parent_keys(self):
-#         parent = HierarchyDict()
-#         parent['one'] = 1
-#         parent['two'] = 2
-
-#         dictionary = HierarchyDict(parent)
-#         dictionary['two'] = 'two'
-#         dictionary['three'] = 'three'
-
-#         msg = 'dictionary should return all keys'
-#         self.assertEqual(sorted(dictionary.keys()), sorted(['one', 'two', 'three']), msg)
-
-#     def test_dictionary_items(self):
-#         parent = HierarchyDict()
-#         parent['one'] = 1
-#         parent['two'] = 2
-
-#         dictionary = HierarchyDict(parent)
-#         dictionary['two'] = 'two'
-#         dictionary['three'] = 'three'
-
-#         actual = sorted(dictionary.items())
-#         expected = sorted([('one', 1), ('two', 'two'), ('three', 'three')])
-#         self.assertEqual(actual, expected, 'dictionary should return items')
 
 if __name__ == '__main__':
     main()
