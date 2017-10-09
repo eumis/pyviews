f258cbfb342303dd5ad61b767a295800f904022a
Eugen Misievich <misievich@gmail.com>
2017-10-09 12:46:00 +0300

    added views folder injection

--- pyviews/tk/app.py
+++ pyviews/tk/app.py
@@ -1,4 +1,6 @@
+from os.path import abspath
 from pyviews.core import ioc
 from pyviews.tk.parsing import convert_to_node
 
 ioc.register_value('convert_to_node', convert_to_node)
+ioc.register_value('views_folder', abspath('views'))
