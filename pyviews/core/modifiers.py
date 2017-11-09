14a5304f2d606b243aa1bd1c961958a0354fac07
Eugen Misievich <misievich@gmail.com>
2017-11-09 13:18:03 +0300

    added inject_global modifier

--- pyviews/core/modifiers.py
+++ pyviews/core/modifiers.py
@@ -1,13 +1,19 @@
+from pyviews.core.ioc import inject
 from pyviews.core.reflection import import_path
 from pyviews.core.parsing import Node
 
-def import_global(node: Node, name, path):
+def import_global(node: Node, key, path):
     imported = None
     try:
         imported = import_path(path)
     except ImportError:
-        node.globals[name] = None
-    node.globals[name] = imported
+        node.globals[key] = None
+    node.globals[key] = imported
 
-def set_global(node: Node, name, value):
-    node.globals[name] = value
+@inject('container')
+def inject_global(node: Node, global_key, inject_key, container=None):
+    value = container.get(inject_key)
+    set_global(node, global_key, value)
+
+def set_global(node: Node, key, value):
+    node.globals[key] = value
--- tests/core/modifiers_test.py
+++ tests/core/modifiers_test.py
@@ -3,10 +3,18 @@ from unittest import TestCase, main
 from importlib import import_module
 from tests.utility import case
 from tests.mock import SomeObject
-from pyviews.core.modifiers import import_global, set_global
+from pyviews.core import ioc
+from pyviews.core.modifiers import import_global, set_global, inject_global
 from pyviews.core.parsing import Node
 
 class TestModifiers(TestCase):
+    def setUp(self):
+        self._initial_container = ioc.CONTAINER
+        ioc.CONTAINER = ioc.Container()
+
+    def tearDown(self):
+        ioc.CONTAINER = self._initial_container
+
     @case(Node(None, None), 'key', 'unittest', unittest)
     @case(Node(None, None), 'anotherKey', 'unittest.TestCase', TestCase)
     @case(Node(None, None), 'someKey', 'importlib.import_module', import_module)
@@ -33,6 +41,15 @@ class TestModifiers(TestCase):
         set_global(node, key, value)
         msg = 'set_global should add value to global'
         self.assertEqual(node.globals[key], value, msg)
+
+    @case(Node(None, None), 'global_key', 'inject_key', 1)
+    @case(Node(None, None), 'global_key', 'inject_key', '1')
+    @case(Node(None, None), 'glob_ar', 'array', [])
+    def test_inject_global(self, node, global_key, inject_key, value):
+        ioc.register_value(inject_key, value)
+        inject_global(node, global_key, inject_key)
+        msg = 'inject_global should get value by key from container and add it to node''s globals'
+        self.assertEqual(node.globals[global_key], value, msg)
 
 if __name__ == '__main__':
     main()
