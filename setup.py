37198690293b0c147897e82708fbced96252abf6
Eugen Misievich <misievich@gmail.com>
2017-11-14 16:48:00 +0300

    changed setup and requirements files

--- setup.py
+++ setup.py
@@ -3,19 +3,21 @@ from setuptools import setup, find_packages
 setup(
     name='pyviews',
     version='0.6.0',
-    description='tkinter MVVM tool',
-    long_description='MVVM tool that allows to describe tkinter widgets with xml',
-    author='me',
+    description='Package for creating tkinter applications in declarative way.',
+    author='Eugen Misievich',
+    author_email='misievich@gmail.com',
     license='MIT',
     classifiers=[
         #   2 - Pre-Alpha
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries'
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.6'
     ],
-    keywords='mvvm tkinter tk pyviews',
+    python_requires='>=3.6',
+    keywords='binding tkinter tk pyviews python',
     packages=find_packages(exclude=['sandbox', 'tests']))
