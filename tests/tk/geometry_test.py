8f604a66c3f1905a65e5728dffbee37894f0303a
eumis <misievich@gmail.com>
2017-12-04 17:25:46 +0300

    added geometry tests

--- tests/tk/geometry_test.py
+++ tests/tk/geometry_test.py
@@ -1,7 +1,8 @@
 from unittest import TestCase, main
 from unittest.mock import Mock, call
 from tests.utility import case
-from pyviews.tk.geometry import GridGeometry, PackGeometry
+from pyviews.tk.geometry import GridGeometry, PackGeometry, PlaceGeometry
+from pyviews.tk.geometry import Row, Column
 
 class TestGridGeometry(TestCase):
     def setUp(self):
@@ -70,6 +71,76 @@ class TestPackGeometry(TestCase):
 
         msg = 'pack should be called with arguments passed to geometry'
         self.assertEqual(self._widget.pack.call_args, call(**args), msg)
+
+class TestPlaceGeometry(TestCase):
+    def setUp(self):
+        self._widget = Mock()
+        self._widget.pack = Mock()
+
+    def test_apply(self):
+        geometry = PlaceGeometry()
+        geometry.apply(self._widget)
+
+        msg = 'apply method should call pack method of widget'
+        self.assertTrue(self._widget.place.called, msg)
+        self.assertEqual(self._widget.place.call_count, 1, msg)
+
+    @case({})
+    @case({'expand':True, 'fill':'x'})
+    def test_init(self, args):
+        geometry = PlaceGeometry(**args)
+        geometry.apply(self._widget)
+
+        msg = 'pack should be called with arguments passed to geometry'
+        self.assertEqual(self._widget.place.call_args, call(**args), msg)
+
+    @case({'expand':True})
+    @case({'expand':True, 'fill':'x'})
+    @case({'some_key':'value'})
+    def test_set(self, args):
+        geometry = PlaceGeometry()
+        for key, value in args.items():
+            geometry.set(key, value)
+        geometry.apply(self._widget)
+
+        msg = 'pack should be called with arguments passed to geometry'
+        self.assertEqual(self._widget.place.call_args, call(**args), msg)
+
+class TestRow(TestCase):
+    def setUp(self):
+        self._widget = Mock()
+        self._widget.grid_rowconfigure = Mock()
+
+    @case(0, {'minsize': 10})
+    @case(2, {'weight': 5, 'minsize': 10})
+    def test_apply(self, index, args):
+        row = Row(self._widget, None)
+
+        row.set_attr('index', index)
+        for key, value in args.items():
+            row.set_attr(key, value)
+        row.apply()
+
+        msg = 'Row.apply should be called with arguments set with set_attr'
+        self.assertEqual(self._widget.grid_rowconfigure.call_args, call(index, **args), msg)
+
+class TestColumn(TestCase):
+    def setUp(self):
+        self._widget = Mock()
+        self._widget.grid_columnconfigure = Mock()
+
+    @case(0, {'minsize': 10})
+    @case(2, {'weight': 5, 'minsize': 10})
+    def test_apply(self, index, args):
+        column = Column(self._widget, None)
+
+        column.set_attr('index', index)
+        for key, value in args.items():
+            column.set_attr(key, value)
+        column.apply()
+
+        msg = 'Column.apply should be called with arguments set with set_attr'
+        self.assertEqual(self._widget.grid_columnconfigure.call_args, call(index, **args), msg)
 
 if __name__ == '__main__':
     main()
