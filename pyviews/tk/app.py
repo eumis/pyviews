69106a61e65fca13cda7da6d43eb8b67ca3da5b2
eumis <misievich@gmail.com>
2018-01-05 13:12:11 +0300

    added canvas

--- pyviews/tk/app.py
+++ pyviews/tk/app.py
@@ -7,6 +7,7 @@ from pyviews.tk.modifiers import set_attr
 from pyviews.tk.styles import Style, parse_attrs as parse_style_attrs, init_styles
 from pyviews.tk.geometry import Row, Column, apply_layout
 from pyviews.tk.widgets import Root
+from pyviews.tk import canvas
 
 def register_dependencies():
     ioc.register_value('convert_to_node', convert_to_node)
@@ -21,6 +22,18 @@ def _register_parsing_steps():
     ioc.register_value('parsing_steps', [parse_style_attrs], Style)
     ioc.register_value('parsing_steps', [apply_layout], Row)
     ioc.register_value('parsing_steps', [apply_layout], Column)
+    _register_canvas_parsing_steps()
+
+def _register_canvas_parsing_steps():
+    ioc.register_value('parsing_steps', [parse_attributes, canvas.render], canvas.Arc)
+    ioc.register_value('parsing_steps', [parse_attributes, canvas.render], canvas.Bitmap)
+    ioc.register_value('parsing_steps', [parse_attributes, canvas.render], canvas.Image)
+    ioc.register_value('parsing_steps', [parse_attributes, canvas.render], canvas.Line)
+    ioc.register_value('parsing_steps', [parse_attributes, canvas.render], canvas.Oval)
+    ioc.register_value('parsing_steps', [parse_attributes, canvas.render], canvas.Polygon)
+    ioc.register_value('parsing_steps', [parse_attributes, canvas.render], canvas.Rectangle)
+    ioc.register_value('parsing_steps', [parse_attributes, canvas.render], canvas.Text)
+    ioc.register_value('parsing_steps', [parse_attributes, canvas.render], canvas.Window)
 
 def launch(root_view=None):
     root_view = 'root' if root_view is None else root_view
