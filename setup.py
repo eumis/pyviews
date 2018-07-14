e8f9888dd3628706ef54c106ee5adf24b0d8bfd5
eumis <misievich@gmail.com>
2018-07-14 07:38:12 +0300

    version 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 def setup_package():
     setup(
         name='pyviews',
-        version='0.9.8',
+        version='1.0.0',
         description='Base package for xml views',
         url='https://github.com/eumis/pyviews',
         author='eumis(Eugen Misievich)',
