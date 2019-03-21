d106210a459cc76637edc65d963be3fe037fc423
eumis <misievich@gmail.com>
2019-03-21 18:17:34 +0300

    added test deploy step

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
+'''Pypi packaging setup'''
+
 from setuptools import setup, find_packages
+from pyviews import __version__ as pyviews_version
 
 def setup_package():
+    '''Package setup'''
     setup(
         name='pyviews',
-        version='2.0.0',
+        version=pyviews_version,
         description='Base package for xml views',
         url='https://github.com/eumis/pyviews',
         author='eumis(Eugen Misievich)',
@@ -14,7 +18,7 @@ def setup_package():
             #   3 - Alpha
             #   4 - Beta
             #   5 - Production/Stable
-            'Development Status :: 4 - Beta',
+            'Development Status :: 5 - Production/Stable',
             'Intended Audience :: Developers',
             'Topic :: Software Development :: Libraries',
             'License :: OSI Approved :: MIT License',
@@ -22,7 +26,7 @@ def setup_package():
         ],
         python_requires='>=3.6',
         keywords='binding tkviews tk tkinter pyviews python mvvm',
-        packages=find_packages(exclude=['tests', 'tests.*']))
+        packages=find_packages()
 
 if __name__ == '__main__':
     setup_package()
