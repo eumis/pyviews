29ddd3edeaa5b3800e7e387c819ca2d12822bb88
eumis <misievich@gmail.com>
2019-04-15 17:02:21 +0300

    [2.0.2] version upgrade

--- pyviews/__init__.py
+++ pyviews/__init__.py
@@ -1,3 +1,3 @@
-'''Package for creating tkinter applications in declarative way.'''
+"""Package for creating tkinter applications in declarative way."""
 
-__version__ = '2.0.1'
+__version__ = '2.0.2'
