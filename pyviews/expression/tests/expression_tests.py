fab15b13d10ffb96841ffa9769eb7b9b7746a921
eumis <misievich@gmail.com>
2020-02-20 12:57:14 +0300

    expression tests refactoring

--- pyviews/expression/tests/expression_tests.py
+++ pyviews/expression/tests/expression_tests.py
@@ -8,17 +8,9 @@ from pyviews.expression.expression import execute
 def object_tree_fixture(request):
     expression = Expression("str(vm.vm.int_value) + vm.vm.str_value + vm.str_value + vm.get()")
     root = expression.get_object_tree()
-    str_node = [entry for entry in root.children if entry.key == 'str'][0]
-    vm_node = [entry for entry in root.children if entry.key == 'vm'][0]
-    vm_node_str = [entry for entry in vm_node.children if entry.key == 'str_value'][0]
-    vm_node_vm = [entry for entry in vm_node.children if entry.key == 'vm'][0]
 
-    request.cls.expression = expression
     request.cls.root = root
-    request.cls.str_node = str_node
-    request.cls.vm_node = vm_node
-    request.cls.vm_node_str = vm_node_str
-    request.cls.vm_node_vm = vm_node_vm
+    request.cls.vm_node = [entry for entry in root.children if entry.key == 'vm'][0]
 
 
 @mark.usefixtures('object_tree_fixture')
@@ -34,7 +26,9 @@ class ExpressionTests:
 
     def test_method(self):
         """get_object_tree(): method node should not contain any child"""
-        assert [e.key for e in self.str_node.children] == []
+        str_method_node = [entry for entry in self.root.children if entry.key == 'str'][0]
+
+        assert [e.key for e in str_method_node.children] == []
 
     def test_property(self):
         """get_object_tree(): object node should have children node for property"""
@@ -45,32 +39,48 @@ class ExpressionTests:
         get_object_tree(): object node should not have children
         if there are no properties used in expression
         """
-        assert [e.key for e in self.vm_node_str.children] == []
+        str_value_node = [entry for entry in self.vm_node.children if entry.key == 'str_value'][0]
+
+        assert [e.key for e in str_value_node.children] == []
 
     def test_property_children(self):
         """
         get_object_tree(): object node should have children
         if there are its properties used in expression
         """
-        assert sorted([e.key for e in self.vm_node_vm.children]) == sorted(['int_value', 'str_value'])
+        inner_vm_node = [entry for entry in self.vm_node.children if entry.key == 'vm'][0]
+
+        assert sorted([e.key for e in inner_vm_node.children]) == sorted(['int_value', 'str_value'])
+
+
+_EXPRESSION_TEST_PARAMETERS = [
+    ('', None, None),
+    (' ', {'some_key': 1}, None),
+    ('2 + 2', None, 4),
+    ('some_key', {'some_key': 1}, 1),
+    ('some_key - 1', {'some_key': 1}, 0),
+    ('str(some_key)', {'some_key': 1}, '1'),
+    ('some_key', {'some_key': 'asdf'}, 'asdf'),
+    ('some_key(some_value)', {'some_key': lambda val: val, 'some_value': 'value'}, 'value')
+]
 
 
 class ExecuteTests:
     @staticmethod
-    @mark.parametrize('expression, params, expected', [
-        ('', None, None),
-        (Expression(' '), {'some_key': 1}, None),
-        ('2 + 2', None, 4),
-        (Expression('some_key'), {'some_key': 1}, 1),
-        ('some_key - 1', {'some_key': 1}, 0),
-        (Expression('str(some_key)'), {'some_key': 1}, '1'),
-        ('some_key', {'some_key': 'asdf'}, 'asdf'),
-        (Expression('some_key(some_value)'), {'some_key': lambda val: val, 'some_value': 'value'},
-         'value')
-    ])
-    def test_execute(expression, params, expected):
+    @mark.parametrize('code, params, expected', _EXPRESSION_TEST_PARAMETERS)
+    def test_execute_expression(code, params, expected):
         """execute() should return expression value"""
+        expression = Expression(code)
+
         actual = execute(expression, params)
+
+        assert expected == actual
+
+    @staticmethod
+    @mark.parametrize('code, params, expected', _EXPRESSION_TEST_PARAMETERS)
+    def test_execute_source_code(code, params, expected):
+        """execute() should return expression value"""
+        actual = execute(code, params)
 
         assert expected == actual
 
