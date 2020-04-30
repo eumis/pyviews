945c92e7edf1251ff322d5cca834eec219e09c48
work <asdf>
2020-04-30 18:24:19 +0300

    refactoring

--- pyviews/binding/setup.py
+++ pyviews/binding/setup.py
@@ -1,6 +1,11 @@
+"""Binding setup"""
+
 from injectool import add_singleton
 
-from pyviews.binding import Binder, run_once, bind_setter_to_expression, bind_inline
+from pyviews.binding.binder import Binder
+from pyviews.binding.once import run_once
+from pyviews.binding.expression import bind_setter_to_expression
+from pyviews.binding.inline import bind_inline
 
 
 def use_binding(binder: Binder = None):
